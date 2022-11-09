ccca77ca414aa197881094d787c3936ea8caaac1
Jonast96 <Jon.Astveit@hotmail.com>
2022-11-09 18:44:50 +0100

    asd

--- src/js/api/auth/login.test.js
+++ src/js/api/auth/login.test.js
@@ -1,43 +1,67 @@
-// import { login } from "./login";
+import { login } from "./login";
 
-// const validEmail = "testing@noroff.no";
-// const invalidEmail = "thisnoworking@xd.no";
-// const password = "thisismorethan8";
-// const token = "asdas222314asdasdsag55343";
+const validEmail = "testing@noroff.no";
+const invalidEmail = "thisnoworking@xd.no";
+const password = "asdfghja";
+const token = "asdas222314asdasdsag55343";
 
-// const profile = {
-//     name: "Jon",
-//     email: validEmail,
-// };
+const profile = {
+  name: "Jon",
+  email: validEmail,
+};
 
-// function fetchSuccess(status = 201, statusText = "success") {
-//     return Promise.resolve({
-//         ok: true,
-//         status,
-//         statusText,
-//         json: () => Promise.resolve({ ...profile, token }),
-//     });
-// }
+class LocalStorageMock {
+  constructor() {
+    this.store = {};
+  }
 
-// function fetchFailure(status = 404, statusText = "failed") {
-//     return Promise.resolve({
-//         ok: false,
-//         status,
-//         statusText,
-//     });
-// }
+  clear() {
+    this.store = {};
+  }
 
-// describe("login", () => {
-//     it("returns a valid access token in local storage and valid response object", async () => {
-//         global.fetch = jest.fn(() => fetchSuccess());
-//         const data = await login(validEmail, password);
-//         expect(validEmail).toMatch("@noroff.no");
-//         expect(password).toHaveLength(8);
-//         expect(data.token).toEqual(token);
-//     });
+  getItem(key) {
+    return this.store[key] || null;
+  }
 
-//     it("throws an error when provided with invalid credentials", async () => {
-//         global.fetch = jest.fn(() => fetchFailure());
-//         await expect(login(invalidEmail, password)).rejects.toThrow("failed");
-//     });
-// });
+  setItem(key, value) {
+    this.store[key] = String(value);
+  }
+
+  removeItem(key) {
+    delete this.store[key];
+  }
+}
+
+global.localStorage = new LocalStorageMock();
+
+function fetchSuccess(status = 201, statusText = "success") {
+  return Promise.resolve({
+    ok: true,
+    status,
+    statusText,
+    json: () => Promise.resolve({ ...profile, token }),
+  });
+}
+
+function fetchFailure(status = 404, statusText = "failed") {
+  return Promise.resolve({
+    ok: false,
+    status,
+    statusText,
+  });
+}
+
+describe("login", () => {
+  it("returns a valid access token in local storage and valid response object", async () => {
+    global.fetch = jest.fn(() => fetchSuccess());
+    const data = await login(validEmail, password);
+    expect(validEmail).toMatch("@noroff.no");
+    expect(password).toHaveLength(8);
+    expect(data.token).toEqual(token);
+  });
+
+  it("throws an error when provided with invalid credentials", async () => {
+    global.fetch = jest.fn(() => fetchFailure());
+    await expect(login(invalidEmail, password)).rejects.toThrow("failed");
+  });
+});
