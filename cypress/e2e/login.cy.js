d27b45300c3b44267634c1cba625cdc62ff56021
Jonast96 <Jon.Astveit@hotmail.com>
2022-11-11 22:05:52 +0100

    { force: true }

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -9,14 +9,14 @@ describe("Authentication", () => {
     const password = "Password";
     cy.visit("/");
     cy.wait(1000);
-    cy.get(".btn-close:visible").click();
-    cy.get("button[data-auth='login']:visible").click();
+    cy.get(".btn-close:visible").click({ force: true });
+    cy.get("button[data-auth='login']:visible").click({ force: true });
     cy.wait(1500);
     cy.get("input[type='email']:visible").should("exist").type(`${email}`);
     cy.get("input[type='password']:visible")
       .should("exist")
       .type(`${password}`);
-    cy.get(".btn-success:visible").click();
+    cy.get(".btn-success:visible").click({ force: true });
     cy.wait(3000);
     cy.then(
       () => expect(window.localStorage.getItem("profile")).to.not.be.null
@@ -29,8 +29,8 @@ describe("Authentication", () => {
     const password = "Password";
     cy.visit("/");
     cy.wait(1000);
-    cy.get(".btn-close:visible").click();
-    cy.get("button[data-auth='login']:visible").click();
+    cy.get(".btn-close:visible").click({ force: true });
+    cy.get("button[data-auth='login']:visible").click({ force: true });
     cy.wait(1500);
     cy.get("input[type='email']:visible")
       .should("exist")
@@ -38,7 +38,7 @@ describe("Authentication", () => {
     cy.get("input[type='password']:visible")
       .should("exist")
       .type(`${password}`);
-    cy.get(".btn-success:visible").click();
+    cy.get(".btn-success:visible").click({ force: true });
     cy.wait(3000);
     cy.then(() => expect(window.localStorage.getItem("profile")).to.be.null);
     cy.then(() => expect(window.localStorage.getItem("token")).to.be.null);
@@ -50,14 +50,14 @@ describe("Authentication", () => {
     const password = "Password";
     cy.visit("/");
     cy.wait(1000);
-    cy.get(".btn-close:visible").click();
-    cy.get("button[data-auth='login']:visible").click();
+    cy.get(".btn-close:visible").click({ force: true });
+    cy.get("button[data-auth='login']:visible").click({ force: true });
     cy.wait(1500);
     cy.get("input[type='email']:visible").should("exist").type(`${email}`);
     cy.get("input[type='password']:visible")
       .should("exist")
       .type(`${password}`);
-    cy.get(".btn-success:visible").click();
+    cy.get(".btn-success:visible").click({ force: true });
     cy.wait(3000);
     cy.then(() => expect(window.localStorage.getItem("profile")).to.exist);
     cy.then(() => expect(window.localStorage.getItem("token")).to.exist);
